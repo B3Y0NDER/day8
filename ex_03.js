dce79a6738925c80568ec10587457cff87a44ef1
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-09 15:03:29 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+const elements = [
+{ id: "image", content: "C'est une image." },
+{ id: "square", content: "C'est un carré." },
+{ id: "sound", content: "C'est un son." },
+{ id: "button", content: "C'est un bouton." },
+{ id: "video", content: "C'est une vidéo." }
+];
+
+elements.forEach(function(item) {
+tippy(`#${item.id}`, {
+content: item.content,
+trigger: 'click',
+onHide(instance) {
+instance.unmount();
+}
+});
+});
+});
