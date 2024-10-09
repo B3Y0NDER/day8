21b4c2655573f907e37e42d0033af4246f4687ac
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-09 14:45:53 +0200

    Update ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-new Splide('#latest-creations-carousel', {
+var splide = new Splide('#latest-creations-carousel', {
 type: 'loop',
 perPage: 3,       
-autoplay: true,   
+autoplay: true,    
 interval: 5000,   
-focus: 'center',  
-gap: '1rem',      
-padding: '5rem'   
-}).mount();
+focus: 'center',   
+});
+splide.mount();      
 });
