6f2cfb6814bdaf2287ce049c4e2e19be4c2b4505
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-09 15:08:51 +0200

    Update ex_04-bis.js

--- ex_04-bis.js
+++ ex_04-bis.js
@@ -1,1 +1,15 @@
+function houdini(name) {
+$("#" + name).hide();  
+$("." + name).hide();  
+}
+
+$(document).ready(function() {
+$("#hide-test").click(function() {
+houdini("test");  
+});
+$("#hide-unique").click(function() {
+
+houdini("unique");  
+});
+});
 
